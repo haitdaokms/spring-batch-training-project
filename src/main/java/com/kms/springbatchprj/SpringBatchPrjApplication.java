e6f4fe64b2a713763e76f892cfbdc4155d3791d5
haitdao <haitdao@kms-technology.com>
2022-10-10 11:33:04 +0700

    init commit

---
+++ src/main/java/com/kms/springbatchprj/SpringBatchPrjApplication.java
@@ -0,0 +1,13 @@
+package com.kms.springbatchprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchPrjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kms/springbatchprj/SpringBatchPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kms.springbatchprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
